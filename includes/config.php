94e3f4015c34d63d5732b6b6a5a2ff31950033e2
Mau <49764953+T-Carri@users.noreply.github.com>
2024-04-29 16:52:33 -0600

    Delete includes/config.php

--- includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_HOST', '198.59.144.15');
-    define('DB_USER', 'jassosmx_darkNight');
-    define('DB_PASSWORD', 'Fs)Fx%-y1kmD' );
-    define('DB_NAME',  'jassosmx_sij');
-    define('SITE_NAME', '');
-?>
